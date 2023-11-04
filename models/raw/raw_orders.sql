ae9c5c703a0a0570e9d0682de1bf0fc072ef024f
Felix Haba <felixhr2000@gmail.com>
2023-11-04 17:52:28 -0500

    Use ephemeral storage and output to parquet in S3

--- models/raw/raw_orders.sql
+++ models/raw/raw_orders.sql
@@ -1,4 +1,4 @@
 select id as order_id,
     user_id as customer_id,
     order_date,
-    status from 's3://dbt-tutorial-public/jaffle_shop_orders.csv'
+    status from {{source('dbt_tutorial', 'jaffle_shop_orders')}}
--- models/raw/raw_payments.sql
+++ models/raw/raw_payments.sql
@@ -1,1 +1,2 @@
+-- Note that defining sources explicitely is optional.
 select * from 's3://dbt-tutorial-public/stripe_payments.csv'
